f23d4453de88072eea2464119dce63c1dba307e1
c-hano <wdq0304@naver.com>
2025-04-26 11:47:42 +0900

    [Chore] initial setting

---
+++ src/test/java/com/example/dotify/DotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
